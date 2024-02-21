3928d254c21e4b62b5ba447860f8c5a71229edf5
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-02-21 10:20:17 -0400

    spread operator y rest

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -2,5 +2,36 @@
 
 let fruits=["Apple","Banana"]
 let [a,b]=fruits
+//con ecmascript
 console.log(a,b)
+//anteriormente
 console.log(a,fruits[1])
+//con objetos destructuring
+let user={
+    username:"Oscar",
+    age:34
+}
+let {username,age}=user
+console.log(username,age)
+//antes
+console.log(user.username,user.age)
+
+//spread operator 
+
+let person={name:"Oscar",age:28}
+let country="MX"
+
+let data={...person,country}
+console.log(data)
+
+let data1={id:1,...person,country}
+console.log(data1)
+
+//rest
+function sum(num,...values){
+    console.log(values)
+    console.log(num+values[0])
+    return num+values[0]
+}
+sum(1,1,2,3)
+//con values podemos asignar varios valores en una sola entrada
