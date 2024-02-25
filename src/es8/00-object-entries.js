69c59b80a137eb80f67f29d80f16ea6889354316
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-02-24 21:00:21 -0400

    obect-entries y object-values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,9 @@
+const countries={
+    MX:"Mexico",
+    CO:"Colombia",
+    CL:"Chile",
+    PE:"Peru"}
+
+console.log(Object.entries(countries))
+//convierte esto en un array de arrays donde cada clave valor es un array
+
---
+++ src/es8/01-object.values.js
@@ -0,0 +1,8 @@
+const countries={
+    MX:"Mexico",
+    CO:"Colombia",
+    CL:"Chile",
+    PE:"Peru"
+}
+console.log(Object.values(countries))
+//Devuelve en un array solo los valores de cada clave valor
