514582c139f1094687bdb398255212dc219bcfa0
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-02-24 21:14:14 -0400

    string padding y trailing commas

---
+++ src/es8/03-string-paddding.js
@@ -0,0 +1,5 @@
+const string = "Hello"
+console.log(string.padStart(7,"hi"))
+//agrega la cadena de texto, el numero indica el numero de caracteres que tendra al final nuestra cadena de caracteres y lo rellena con el hi
+
+console.log(string.padEnd(6,"_"))
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,5 @@
+const array=[24,34,25,24, , , , , ,]
+console.log(array)
+console.log(array.length)
+
+//si hay comas con espacios vacios en los arrays los cuenta como posiciones y ademas se detalla los elementos vacios como "empty items"
