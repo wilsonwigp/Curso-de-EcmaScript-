23ce994febcd9173ff15defc0c173ec883daa9a5
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-02-25 22:10:36 -0400

    flatmap y trim1

--- src/es10/00-flat-map.js
+++ src/es10/00-flat-map.js
@@ -1,4 +1,8 @@
-//devuelve una matriz de cualquier submatriz
-
+//devuelve una matriz de cualquier submatriz aplanamiento de matrices
+//flat
 const array=[1,1,2,3,4,[1,2,3,4,[1,2,3]]]
 console.log(array.flat(3))
+
+//flatmap usando una funcion
+const array2=[1,2,3,4,5]
+console.log(array2.flatMap(v=>[v,v*2]))
