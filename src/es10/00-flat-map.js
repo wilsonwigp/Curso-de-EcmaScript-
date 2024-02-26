3ded181de42dbf2fb4192331d887149b18975549
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-02-25 22:10:07 -0400

    flatmap y trim

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,4 @@
+//devuelve una matriz de cualquier submatriz
+
+const array=[1,1,2,3,4,[1,2,3,4,[1,2,3]]]
+console.log(array.flat(3))
---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,5 @@
+const hello="                Hello World         "
+console.log(hello.trimStart())
+console.log(hello.trimEnd())
+console.log(hello.trim())
+//elimina los espacios de principio y final
