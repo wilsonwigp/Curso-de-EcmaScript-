b651760d8cbbc8fd38e4011de9cc6989da59ae4b
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-03-04 23:20:55 -0400

    globalthis y matchall

---
+++ src/es11/04-globalthis.js
@@ -0,0 +1,5 @@
+console.log(window)//navegador
+//para no tener conflictos en ciertos comandos que se puedan leer tanto en el navegador, local y servidor
+console.log(global)//node
+console.log(selft)//webworker
+console.log(globalThis)
