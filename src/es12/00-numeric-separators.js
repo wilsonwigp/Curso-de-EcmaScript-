ed0360ced29c2040395e588a24afba1a178cf95a
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-03-05 22:24:20 -0400

    Numeric-separators y replaceAll

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,3 @@
+const value=100_000_000_000
+console.log(value)
+//sepuede separar un numero grande por _ para mejor entnedimiento y no afecta en las opoeraciones computacionales
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,4 @@
+const string="Javascript es un maravilloso lenguaje de programacion"
+const replacedString=string.replace("Javascript","typescript")
+console.log(replacedString)
+//cambia la frase javascript por typescript
