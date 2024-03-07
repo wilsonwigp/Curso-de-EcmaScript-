7a42b9edd5025c0c87e2236ffd96fcbac6ed14aa
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-03-06 21:09:29 -0400

    at

---
+++ src/es13/00-at.js
@@ -0,0 +1,5 @@
+const array=["one","two","three","four","five","six"]
+
+console.log(array[array.length-1])
+console.log(array.at(-1))
+//recorrer los elementos sin conocer cuantos tiene para llegar a la ultima posicion 
