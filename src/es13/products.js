3556d96e5f6aeb4f5cc6d8fcd94f5554069d0917
Wilson Gutierrez <wilsonwigp@gmail.com>
2024-03-07 08:31:23 -0400

    top-level-await

---
+++ src/es13/products.js
@@ -0,0 +1,8 @@
+import fetch from"node-fetch";
+
+const response = await fetch("https://api.escuelajs.co/api/v1/products");
+const products = await response.json();
+
+export { products };
+
+//ya no es necesario la palabra async
